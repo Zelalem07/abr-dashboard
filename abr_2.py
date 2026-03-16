6149cfec04aa4ac3309f5ba96d28d48e61164092
Zelalem07 <zelalemfikadu11@gmail.com>
2026-03-16 15:32:58 +0300

    First commit - Streamlit dashboard

--- abr_2.py
+++ abr_2.py
@@ -1,7 +1,1 @@
-import pandas as pd
-import plotly.express as px
-import streamlit as st
-
-st.set_page_config(layout="wide")
-st.title("Abaarso Dashboard")
-df = pd.read_csv("Master_infraction_data__.csv")
+# Test
